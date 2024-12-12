7e3f53ac348a4112ec3f0e225334f5ec1b2c9aa5
corrze <107592289+corrze@users.noreply.github.com>
2024-12-12 13:24:35 -0800

    Drawing panels and stuf

--- battle_screen_1.py
+++ battle_screen_1.py
@@ -1,5 +1,8 @@
 import pygame
 
+clock = pygame.time.Clock()
+fps = 60
+
 pygame.init()
 
 #window for game
@@ -15,4 +18,34 @@ pygame.display.set_caption('Battle 1')
 #Background image
 background_img = pygame.image.load('img/Background/FreeBackground.png').convert_alpha()
 #Panel Image
-#panel_img = pygame.image.load('img/)
+panel_img = pygame.image.load('img/Icons/FreePanel.png').convert_alpha()
+
+
+#Function for drawingBackgound 
+
+def draw_background():
+    screen.blit(background_img,(0,0))
+    
+    
+#Function for drawing panel
+def draw_panel():
+    screen.blit(panel_img, (0, screen_height - bottom_panel1))
+    
+run = True
+while run:
+
+    clock.tick(fps)
+    
+    #Draw background
+    draw_background()
+    
+    #Draw Panel
+    draw_panel()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    pygame.display.update()
+    
+pygame.quit()
