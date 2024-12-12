071b1ea7fa3ecc2366a4464cc121a762756191d8
corrze <107592289+corrze@users.noreply.github.com>
2024-12-12 13:07:00 -0800

    Background image

--- battle_screen_1.py
+++ battle_screen_1.py
@@ -10,3 +10,9 @@ screen_height = 700 + bottom_panel1
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Battle 1')
 
+
+#Loading an image
+#Background image
+background_img = pygame.image.load('img/Background/FreeBackground.png').convert_alpha()
+#Panel Image
+#panel_img = pygame.image.load('img/)
