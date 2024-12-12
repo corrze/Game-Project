fda0c680a601ef21c411391266e57876f303dec4
corrze <107592289+corrze@users.noreply.github.com>
2024-12-12 12:51:47 -0800

    Start code

---
+++ battle_screen_1.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+#window for game
+bottom_panel1 = 300
+screen_width = 1600
+screen_height = 700 + bottom_panel1
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Battle 1')
+
