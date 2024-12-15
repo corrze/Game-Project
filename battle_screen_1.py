83e0c66783b4693e069b75a0b2e953b58362c089
unknown <corrze620@gmail.com>
2024-12-15 03:33:28 -0800

    Added placeholder bears

--- battle_screen_1.py
+++ battle_screen_1.py
@@ -42,46 +42,62 @@ def draw_panel():
 
 #Knight Class
 class Knight():
-    def __init__(self, x, y, name, max_hp, str, int, dex, potions):
+    def __init__(self, x, y, name, max_hp, str, potions):
          self.name = name
          self.max_hp = max_hp
          self.hp = max_hp
          self.atk = str
          self.start_potions = potions
          self.alive = True
-         self.image = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.png")
-         #self.rect = self.image.get_rect()
+         img = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.jpg")
+         self.image = pygame.transform.scale(img, (img.get_width() / 3, img.get_height() / 3))
+         self.rect = self.image.get_rect()
          self.rect.center = (x, y)
+         
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
 
 class Mage():
-    def __init__(self, x, y, name, max_hp, str, int, dex, potions):
+    def __init__(self, x, y, name, max_hp, int, potions):
         self.name = name
         self.max_hp = max_hp
         self.hp = max_hp
         self.atk = int
         self.start_potions = potions
         self.alive = True
-        self.image = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.png")
+        img = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.jpg")
+        self.image = pygame.transform.scale(img, (img.get_width() / 3, img.get_height() / 3))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+    
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
 class Archer():
-    def __init__(self, x, y, name, max_hp, str, int, dex, potions):
+    def __init__(self, x, y, name, max_hp, dex, potions):
         self.name = name
         self.max_hp = max_hp
         self.hp = max_hp
         self.atk = dex
         self.start_potions = potions
         self.alive = True
-        self.image = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.png")
+        img = pygame.image.load(f"img/Classes/{self.name}/Placeholderbear1.jpg")
+        self.image = pygame.transform.scale(img, (img.get_width() / 3, img.get_height() / 3))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         
     def draw(self):
         screen.blit(self.image, self.rect)
         
-knight = Knight(200, 260, 'Knight', 30, 10, 0, 0, 3)
+knight = Knight(200, 260, 'Knight', 30, 10, 3)
+mage = Mage(550, 270, 'mage', 30, 10, 3)
+archer = Archer(700, 270, 'archer', 30, 10, 3)
+
+hero_list = []
+hero_list.append(knight)
+hero_list.append(mage)
+hero_list.append(archer)
 
 run = True
 while run:
@@ -94,8 +110,9 @@ while run:
     #Draw Panel
     draw_panel()
     
-    #draw knight
-    knight.draw()
+    #draw Heros
+    for hero in hero_list:
+        hero.draw()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
